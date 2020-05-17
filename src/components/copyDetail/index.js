9fa3e3860990deb250179d2bc9b3d2e4d98edb50
Arlenys Fgueiras <arlenys.virgen@gmail.com>
2020-05-17 00:19:51 -0400

    add arrow detail component

--- src/components/copyDetail/index.js
+++ src/components/copyDetail/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from '../Card/index';
 import Layout from '../Layout/index';
 import Boton from '../buttons/index';
+import NavigateBefore from '@material-ui/icons/NavigateBefore';
+import NavigateNext from '@material-ui/icons/NavigateNext';
 
 const Detail = (props) =>{
     const { color, year, pantone } = props.match.params;
@@ -12,11 +14,15 @@ const Detail = (props) =>{
             text = '¡Copiado!'
             year = {year}
             pantone = {pantone}
-            style ={{ backgroundColor:`#${color}`}}
+            style ={{ backgroundColor:`#${color}`,marginBottom:'2px'}}
             />
             <div className='action'>
-                <Boton> Anterior</Boton>
-                <Boton> Siguiente</Boton>
+                <div className='boton'>
+                    <NavigateBefore className='icon'  onClick={back}/><Boton onClick={back}> Anterior</Boton>
+                </div>
+                <div className='boton'>
+                    <Boton onClick={next}> Siguiente</Boton><NavigateNext className='icon' onClick={next}/>
+                </div>
             </div>
         </Layout> 
 
